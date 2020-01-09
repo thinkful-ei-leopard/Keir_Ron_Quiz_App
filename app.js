d184b917bbb469a2468472fdd5f0d28f97ddfa34
Ron Martin <ronmartin89@gmail.com>
2020-01-09 13:18:59 -0800

    Created the 5 questions and answers needed

--- app.js
+++ app.js
@@ -5,24 +5,54 @@ const store = {
     // 5 or more questions are required
     questions: [
       {
-        question: 'What color is broccoli?',
+        question: 'Which of these is not an alignment type?',
         answers: [
-          'red',
-          'orange',
-          'pink',
-          'green'
+          'Chaotic good',
+          'Neutral',
+          'Lawful evil',
+          'Radical'
         ],
-        correctAnswer: 'green'
+        correctAnswer: 'Radical'
       },
       {
-        question: 'What is the current year?',
+        question: 'Which class gets the most out of Skills?',
         answers: [
-          '1970',
-          '2015',
-          '2019',
-          '2005'
+          'Wizard',
+          'Druid',
+          'Rogue',
+          'Cleric'
         ],
-        correctAnswer: '2019'
+        correctAnswer: 'Rogue'
+      },
+      {
+        question: 'Which of these is not a playable Race in the Player\'s Handbook (5th Edition)?',
+        answers: [
+          'Dragonborn',
+          'Orc',
+          'Dwarf',
+          'Gnome'
+        ],
+        correctAnswer: 'Orc'
+      },
+      {
+        question: 'Which of these dice are used for an Attack Roll?',
+        answers: [
+          'D4',
+          'D8',
+          'D10',
+          'D20'
+        ],
+        correctAnswer: 'D20'
+      },
+      {
+        question: 'How much Wisdom is needed for a +2 Modifier?',
+        answers: [
+          '2',
+          '5',
+          '10',
+          '14'
+        ],
+        correctAnswer: '14'
       }
     ],
     questionNumber: 0,
@@ -41,3 +71,8 @@ const store = {
    * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
    *
    */
+
+
+   // We might want to use a factory function (or classes) and closure to create
+   // instances of the "The Question Page" since it will be very simliar, but with 
+   // different data inside
