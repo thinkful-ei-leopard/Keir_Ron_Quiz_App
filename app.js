fffc0fe0e991fc119b1352b8b6425ed7a879f7a1
Ron Martin <ronmartin89@gmail.com>
2020-01-10 09:33:05 -0800

    Put updated HTML wireframe into JS (commented out until we use it)

--- app.js
+++ app.js
@@ -1,64 +1,64 @@
 'use strict';
 
 const store = {
-    // 5 or more questions are required
-    questions: [
-      {
-        question: 'Which of these is not an alignment type?',
-        answers: [
-          'Chaotic good',
-          'Neutral',
-          'Lawful evil',
-          'Radical'
-        ],
-        correctAnswer: 'Radical'
-      },
-      {
-        question: 'Which class gets the most out of Skills?',
-        answers: [
-          'Wizard',
-          'Druid',
-          'Rogue',
-          'Cleric'
-        ],
-        correctAnswer: 'Rogue'
-      },
-      {
-        question: 'Which of these is not a playable Race in the Player\'s Handbook (5th Edition)?',
-        answers: [
-          'Dragonborn',
-          'Orc',
-          'Dwarf',
-          'Gnome'
-        ],
-        correctAnswer: 'Orc'
-      },
-      {
-        question: 'Which of these dice are used for an Initiative Roll?',
-        answers: [
-          'D4',
-          'D8',
-          'D10',
-          'D20'
-        ],
-        correctAnswer: 'D20'
-      },
-      {
-        question: 'How much Wisdom is needed for a +2 Modifier?',
-        answers: [
-          '2',
-          '5',
-          '10',
-          '14'
-        ],
-        correctAnswer: '14'
-      }
-    ],
-    questionNumber: 0,
-    score: 0
-  };
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'Which of these is not an alignment type?',
+      answers: [
+        'Chaotic good',
+        'Neutral',
+        'Lawful evil',
+        'Radical'
+      ],
+      correctAnswer: 'Radical'
+    },
+    {
+      question: 'Which class gets the most out of Skills?',
+      answers: [
+        'Wizard',
+        'Druid',
+        'Rogue',
+        'Cleric'
+      ],
+      correctAnswer: 'Rogue'
+    },
+    {
+      question: 'Which of these is not a playable Race in the Player\'s Handbook (5th Edition)?',
+      answers: [
+        'Dragonborn',
+        'Orc',
+        'Dwarf',
+        'Gnome'
+      ],
+      correctAnswer: 'Orc'
+    },
+    {
+      question: 'Which of these dice are used for an Initiative Roll?',
+      answers: [
+        'D4',
+        'D8',
+        'D10',
+        'D20'
+      ],
+      correctAnswer: 'D20'
+    },
+    {
+      question: 'How much Wisdom is needed for a +2 Modifier?',
+      answers: [
+        '2',
+        '5',
+        '10',
+        '14'
+      ],
+      correctAnswer: '14'
+    }
+  ],
+  questionNumber: 0,
+  score: 0
+};
   
-  /**
+/**
    *
    * Your app should include a render() function, that regenerates
    * the view each time the store is updated. See your course
@@ -70,11 +70,10 @@ const store = {
    * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
    *
    */
-
-
-   // We might want to use a factory function (or classes) and closure to create
-   // instances of the "The Question Page" since it will be very simliar, but with 
-   // different data inside
+ 
+// We might want to use a factory function (or classes) and closure to create
+// instances of the "The Question Page" since it will be very simliar, but with 
+// different data inside
 
 
 // Wireframe for "Intro" View
@@ -88,30 +87,33 @@ const store = {
 
 // The wireframe for the "Question" View
 // =========================================================================
-//    <div class="container">
-//    <form>
-//    <fieldset>
-//        <legend>Choose your favorite monster</legend>
-//        <!-- Added BRs to the ends of each radio button so they stack
-//             We can either do that or put every pair in a div-->
-//        <input type="radio" id="kraken" name="monster">
-//        <label for="kraken">Kraken</label><br>
-   
-//        <input type="radio" id="sasquatch" name="monster">
-//        <label for="sasquatch">Sasquatch</label><br>
-   
-//        <input type="radio" id="mothman" name="monster">
-//        <label for="mothman">Mothman</label><br>
+// <div class="container">
+// <form>
+// <fieldset>
+//     <legend>Which of these dice are used for an Initiative roll?</legend>
+//     <div>
+//         <input type="radio" id="kraken" name="monster">
+//         <label for="kraken">D4</label>
+//     </div>
+//     <div>
+//         <input type="radio" id="sasquatch" name="monster">
+//         <label for="sasquatch">D8</label>
+//     </div>
+//     <div>
+//         <input type="radio" id="mothman" name="monster">
+//         <label for="mothman">D10</label>
+//     </div>
+//     <div>
+//         <input type="radio" id="dragon" name="monster">
+//         <label for="dragon">D20</label>
+//     </div>
+    
+//     <button type="submit">Submit</button>
+// </fieldset>
+// <img src="/images/dice.jpg" alt="numerous dice">
+// </form>
 
-//        <input type="radio" id="dragon" name="monster">
-//        <label for="dragon">Dragon</label><br>
-       
-//        <button type="submit">Submit</button>
-//    </fieldset>
-//    <img src="/Images/dice.jpg" alt="numerous dice">
-//    </form>
-   
-//    <h2>Question 2 of 5</h2>
+// <h2>Question 2 of 5</h2>
 // </div>
 
 // The wireframe for "Correct" feedback View
@@ -119,7 +121,7 @@ const store = {
 // <div class="container">
 // <h2>Correct!</h2>
 
-// <img src="/Images/correct.svg" alt="">
+// <img src="/images/correct.svg" alt="">
 
 // <section>
 //     <p>Current Score</p>
